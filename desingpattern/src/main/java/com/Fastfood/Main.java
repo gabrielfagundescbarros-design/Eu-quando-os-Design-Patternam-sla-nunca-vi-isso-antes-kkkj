3b6bc2bae69888b93ca038eee4bc1959937f2bda
Toboco 🦴 <gabrielfagundescbarros@gmail.com>
2025-10-20 22:33:57 -0300

    Verificação extra final

--- desingpattern/src/main/java/com/Fastfood/Main.java
+++ desingpattern/src/main/java/com/Fastfood/Main.java
@@ -3,7 +3,7 @@ package com.Fastfood;
 import java.util.Scanner;
 
 
-public class Main {1
+public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         
